13b8630c4e8c9aa65f7892974cb0c14791997cfe
Belu-cat <momo.ahsan.family@gmail.com>
2023-09-09 23:04:05 -0400

    hide the line

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ tersprite.sprite("default", 0, 5)
 # print(tersprite.sprites)
 # print(tersprite.spriteLoc("default"))
 var = tersprite.deconstruct(tersprite.drawSprite("default"), 60, 60)
-i = 0
+i = 1
 while i != len(var):
     print(var[i])
     i += 1
