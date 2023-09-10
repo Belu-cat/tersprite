e461a40985de9df1ce74791637b88c9cccda3e9d
Belu-cat <momo.ahsan.family@gmail.com>
2023-09-09 23:09:18 -0400

    changed image size

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tersprite
+import os
 tersprite.assetLoc("assets")
 # print(tersprite.assetLoc)
 tersprite.sprite("default", 0, 5)
 # print(tersprite.sprites)
 # print(tersprite.spriteLoc("default"))
-var = tersprite.deconstruct(tersprite.drawSprite("default"), 60, 60)
+var = tersprite.deconstruct(tersprite.drawSprite("default"), 30, 25)
 i = 1
 while i != len(var):
     print(var[i])
